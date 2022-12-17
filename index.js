f8ff3eafe3f52c8d146d9ba8fff9e352a6ad88b5
rafeeqpk <rafeeqpk36@gmail.com>
2022-12-17 13:17:35 +0530

    simple node examples

---
+++ index.js
@@ -0,0 +1,22 @@
+var rect={
+    perimeter:(x,y)=>(2*(x+y)),
+    area:(x,y)=>(x*y)
+}
+
+function solveRect(l,b){
+    console.log("Solving for rectangle with l= " +l + " and b= "+b)
+    if(l<=0 || b<=0){
+        console.log("Rectangle dimension should be greater than zero:l="+l+",and b="+b)
+    }
+    else {
+        console.log("Area of the rectangle is = " + rect.area(l,b))
+        console.log("Perimeter of Rectangle is = "+ rect.perimeter(l,b))
+    }
+
+}
+
+solveRect(2,5)
+solveRect(10.5,10)
+solveRect(0,9)
+solveRect(-9,8)
+solveRect(-8,-7)
